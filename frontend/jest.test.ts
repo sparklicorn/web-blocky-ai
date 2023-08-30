84fb566289bb326fddefb3748a05bef5216cf80e
Jeff <sparklicorn@users.noreply.github.com>
2023-08-30 16:58:06 -0500

    Add Jest and supporting packages

---
+++ frontend/jest.test.ts
@@ -0,0 +1,10 @@
+// TODO Remove this file any time later. It's just here to make sure Jest is working.
+
+// Imports work
+import routes from './routes';
+
+describe('Jest', () => {
+  it('works', () => {
+    expect(true).toBe(true);
+  });
+});
