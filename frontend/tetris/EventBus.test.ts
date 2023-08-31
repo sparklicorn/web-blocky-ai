c616d8588e3441fa8252ac3c5806ca077979efbe
Jeff <sparklicorn@users.noreply.github.com>
2023-08-31 17:07:12 -0500

    Add tests for Event, EventBus

---
+++ frontend/tetris/EventBus.test.ts
@@ -0,0 +1,87 @@
+import { Event } from "./Event";
+import EventBus from "./EventBus";
+
+describe('EventBus', () => {
+  let eventBus: EventBus;
+  let mockListener: jest.Mock;
+
+  beforeEach(() => {
+    eventBus = new EventBus();
+    mockListener = jest.fn();
+  });
+
+  describe('hasListeners', () => {
+    test('returns false if the event has no listeners', () => {
+      expect(eventBus.hasListeners('test')).toEqual(false);
+    });
+
+    test('returns true if the event has listeners', () => {
+      eventBus.registerEventListener('test', mockListener);
+      expect(eventBus.hasListeners('test')).toEqual(true);
+    });
+  });
+
+  describe('registerEventListener', () => {
+    test('registers an event listener', () => {
+      eventBus.registerEventListener('test', mockListener);
+      expect(eventBus.hasListeners('test')).toEqual(true);
+    });
+
+    test('returns true', () => {
+      expect(eventBus.registerEventListener('test', mockListener)).toEqual(true);
+    });
+  });
+
+  describe('unregisterEventListener', () => {
+    test('unregisters an event listener', () => {
+      eventBus.registerEventListener('test', mockListener);
+      eventBus.unregisterEventListener('test', mockListener);
+      expect(eventBus.hasListeners('test')).toEqual(false);
+    });
+
+    test('returns true if a listener was unregistered', () => {
+      eventBus.registerEventListener('test', mockListener);
+      expect(eventBus.unregisterEventListener('test', mockListener)).toEqual(true);
+    });
+
+    test('returns false if a listener was not unregistered', () => {
+      expect(eventBus.unregisterEventListener('test', mockListener)).toEqual(false);
+    });
+  });
+
+  describe('unregisterAllEventListeners', () => {
+    test('unregisters all event listeners', () => {
+      eventBus.registerEventListener('test', mockListener);
+      eventBus.unregisterAllEventListeners('test');
+      expect(eventBus.hasListeners('test')).toEqual(false);
+    });
+
+    test('returns true if any listeners were unregistered', () => {
+      eventBus.registerEventListener('test', mockListener);
+      expect(eventBus.unregisterAllEventListeners('test')).toEqual(true);
+    });
+
+    test('returns false if no listeners were unregistered', () => {
+      expect(eventBus.unregisterAllEventListeners('test')).toEqual(false);
+    });
+  });
+
+  describe('throwEvent', () => {
+    let event: Event;
+
+    beforeEach(() => {
+      event = new Event('test');
+      eventBus.registerEventListener(event.name, mockListener);
+    });
+
+    test('calls all listeners for the given event', () => {
+      eventBus.throwEvent(event);
+      expect(mockListener).toHaveBeenCalledWith(event);
+    });
+
+    test('does not call listeners for other events', () => {
+      eventBus.throwEvent(new Event('other'));
+      expect(mockListener).not.toHaveBeenCalled();
+    });
+  });
+});
--- frontend/tetris/EventBus.ts
+++ frontend/tetris/EventBus.ts
@@ -1,12 +1,35 @@
 import { Event, EventListener } from './Event';
 
+/**
+ * An EventBus contains a registry of event listeners that can be invoked by throwing events.
+ */
 export default class EventBus {
   private _listeners: { [eventName: string]: EventListener[] };
 
+  /**
+   * Creates a new EventBus with no listeners.
+   */
   constructor() {
     this._listeners = {};
   }
 
+  /**
+   * Returns true if the event has listeners for the given event name.
+   *
+   * @param name The event name to check.
+   * @returns True if the event has listeners for the given event name; false otherwise.
+   */
+  hasListeners(name: string): boolean {
+    return name in this._listeners && this._listeners[name].length > 0;
+  }
+
+  /**
+   * Registers an event listener for the given event name.
+   *
+   * @param name The event name to listen for.
+   * @param listener The event listener to register.
+   * @returns True if the event listener was registered; false otherwise.
+   */
   registerEventListener(name: string, listener: EventListener): boolean {
     if (name in this._listeners) {
       this._listeners[name].push(listener);
@@ -17,6 +40,13 @@ export default class EventBus {
     return true;
   }
 
+  /**
+   * Unregisters an event listener for the given event name.
+   *
+   * @param name The event name to unregister.
+   * @param listener The event listener to unregister.
+   * @returns True if the event listener was unregistered; false otherwise.
+   */
   unregisterEventListener(name: string, listener: EventListener): boolean {
     if (name in this._listeners) {
       const index = this._listeners[name].indexOf(listener);
@@ -29,15 +59,28 @@ export default class EventBus {
     return false;
   }
 
+  /**
+   * Unregisters all event listeners for the given event name.
+   *
+   * @param name The event name to unregister.
+   * @returns True if any event listeners were unregistered; false otherwise.
+   */
   unregisterAllEventListeners(name: string): boolean {
     if (name in this._listeners) {
-      this._listeners[name] = [];
-      return true;
+      if (this._listeners[name].length > 0) {
+        this._listeners[name] = [];
+        return true;
+      }
     }
 
     return false;
   }
 
+  /**
+   * Throws the given event to all listeners.
+   *
+   * @param event The event to throw.
+   */
   throwEvent(event: Event): void {
     if (event.name in this._listeners) {
       this._listeners[event.name].forEach(listener => listener(event));
