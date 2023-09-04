22a3d681e8cf39dc08025ab24d72f70a0c3b2631
Jeff <sparklicorn@users.noreply.github.com>
2023-09-04 13:48:15 -0500

    Add ZMod tests

--- frontend/tetris/ZMod.ts
+++ frontend/tetris/ZMod.ts
@@ -2,12 +2,18 @@ export default class ZMod {
   private static readonly ERR_VALUE_NOT_INTEGER = 'Value must be an integer.';
   private static readonly ERR_VALUE_NOT_POSITIVE = 'Value must be positive.';
 
+  /**
+   * Throws an error if the value is not an integer.
+   */
   private static _validateInteger(value: number): void {
     if (!Number.isInteger(value)) {
       throw new Error(ZMod.ERR_VALUE_NOT_INTEGER);
     }
   }
 
+  /**
+   * Throws an error if the value is not a positive integer.
+   */
   private static _validatePositiveInteger(value: number): void {
     ZMod._validateInteger(value);
     if (value <= 0) {
@@ -15,6 +21,9 @@ export default class ZMod {
     }
   }
 
+  /**
+   * Returns the value modulo mod.
+   */
   static apply(value: number, mod: number): number {
     ZMod._validateInteger(value);
     ZMod._validatePositiveInteger(mod);
@@ -25,6 +34,9 @@ export default class ZMod {
   private _value: number = 0;
   readonly mod: number;
 
+  /**
+   * Creates a new ZMod with the given value and mod.
+   */
   constructor(value: number, mod: number) {
     ZMod._validatePositiveInteger(mod);
 
@@ -32,20 +44,38 @@ export default class ZMod {
     this.set(value);
   }
 
+  /**
+   * Returns the value of this ZMod.
+   */
   get(): number {
     return this._value;
   }
 
+  /**
+   * Sets the value of this ZMod.
+   *
+   * @returns This ZMod.
+   */
   set(value: number): ZMod {
     ZMod._validateInteger(value);
     this._value = ((value % this.mod) + this.mod) % this.mod;
     return this;
   }
 
+  /**
+   * Adds the given value to this ZMod.
+   *
+   * @returns This ZMod.
+   */
   add(value: number): ZMod {
     return this.set(this._value + value);
   }
 
+  /**
+   * Subtracts the given value from this ZMod.
+   *
+   * @returns This ZMod.
+   */
   subtract(value: number): ZMod {
     return this.set(this._value - value);
   }
