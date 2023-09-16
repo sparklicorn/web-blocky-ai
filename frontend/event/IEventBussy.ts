93451085c1ee1b5c7d47406667de59a3f9f38aab
Jeff <sparklicorn@users.noreply.github.com>
2023-09-15 19:06:33 -0500

    Events are now frozen before throwing by EventBus

--- frontend/event/IEventBussy.ts
+++ frontend/event/IEventBussy.ts
@@ -4,6 +4,7 @@ import IEventRegistrar from "./IEventRegistrar";
 export default interface IEventBussy extends IEventRegistrar {
   /**
    * Throws the given event to all registered listeners.
+   * The given event will be frozen so that listeners or the thrower cannot further modify it.
    *
    * @param event The event to throw.
    */
